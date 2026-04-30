2f4c58206ea35d5636c8c54a754ae12bdb30f7c4
Meppies <117522863+Meppies@users.noreply.github.com>
2026-04-30 06:00:13 +0200

    chore(card): banner v2.2.10 (matches manifest)

--- custom_components/ns_reisadvies/www/ns-reisadvies-card.js
+++ custom_components/ns_reisadvies/www/ns-reisadvies-card.js
@@ -1307,7 +1307,7 @@ window.customCards.push({
 });
 
 console.info(
-  "%c NS-REISADVIES-CARD %c v2.2.1 ",
+  "%c NS-REISADVIES-CARD %c v2.2.10 ",
   "color: white; background: #003082; font-weight: 700;",
   "color: #003082; background: #FFC917; font-weight: 700;"
 );
